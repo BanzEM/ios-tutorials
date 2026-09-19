2a0afb576bbc0acd135f4c49d18faa3e91dabc33
BanzEM <bandisilemazomba123@gmail.com>
2026-09-19 22:36:55 +0200

    adding color and tabviews

---
+++ DiceView/DiceView/DiceViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceViewApp.swift
+//  DiceView
+//
+//  Created by Bandisile Mazomba on 2026/09/19.
+//
+
+import SwiftUI
+
+@main
+struct DiceViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
--- OnBardingFlow/OnBardingFlow/FeatureCard.swift
+++ OnBardingFlow/OnBardingFlow/FeatureCard.swift
@@ -16,14 +16,24 @@ struct FeatureCard: View {
         HStack {
             Image(systemName: iconName)
                 .font(.largeTitle)
+                .frame(width: 50)
+                .padding(.trailing, 10)
             
             Text(description)
+            
+            Spacer()
                 
         }
         .padding()
-        .background(.tint, in: RoundedRectangle(cornerRadius: 12))
-        .foregroundStyle(.white)
+        .background {
+                    RoundedRectangle(cornerRadius: 12)
+                        .foregroundStyle(.tint)
+                        .opacity(0.9)
+                        .brightness(-0.2)
+                }
+                .foregroundStyle(.white)
     }
+        
 }
 
 #Preview {
